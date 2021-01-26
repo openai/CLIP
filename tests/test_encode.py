c54dedd7392a3d746a239763c96daf7a4255cdea
Jacky <jacky2wong@gmail.com>
2021-01-26 17:41:53 +1100

    Add smoke test

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -2,15 +2,15 @@ import clip
 import torch
 import torch
 from PIL import Image
-def test_simple_cpu():
+def test_smoke_simple_cpu():
     device = 'cpu'
     model, preprocess = clip.load("ViT-B/32", device=device)
     image = preprocess(Image.open('CLIP.png')).unsqueeze(0).to(device)
     text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
-    with torhc.no_grad():
-        assert model.encode_image(image), "Encoding an image does not work"
-        assert model.encode_text(text), "Encoding text does not work"
+    with torch.no_grad():
+        model.encode_image(image)
+        model.encode_text(text)
         logits_per_image, logits_per_text = model(image, text)
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-    print("Label probs:", probs)
+    assert True
 
