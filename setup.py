72af33ae08178a0c67f84aa71b70e3f5e508dc00
Jong Wook Kim <jongwook@openai.com>
2021-01-30 03:00:32 +0900

    trying without explicit package_data

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 
 import pkg_resources
 from setuptools import setup, find_packages
-from pathlib import Path
-
 
 setup(
     name="clip",
@@ -12,7 +10,6 @@ setup(
     description="",
     author="OpenAI",
     packages=find_packages(exclude=["tests*"]),
-    package_data={'clip': [str(x) for x in list(Path('clip').rglob("*.gz"))]},
     install_requires=[
         str(r)
         for r in pkg_resources.parse_requirements(
