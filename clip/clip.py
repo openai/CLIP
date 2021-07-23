cd446fb776a1a2ce80beb4f42ab080d985051228
Santiago Castro <sacastro@umich.edu>
2021-07-22 21:39:13 -0700

    Use tqdm with 1024 instead of 1000 unit scale

--- clip/clip.py
+++ clip/clip.py
@@ -53,7 +53,7 @@ def _download(url: str, root: str = os.path.expanduser("~/.cache/clip")):
             warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
 
     with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True) as loop:
+        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
             while True:
                 buffer = source.read(8192)
                 if not buffer:
