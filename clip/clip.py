a63f881a0158c99c6999eaf133ddb4f3ae0c24e8
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2021-09-10 20:04:12 +0200

    In Compose, replace lambda function with named function
    This prevents the following error on Windows (when using
    a multi-process DataLoader, for example):
    
    AttributeError: Can't pickle local object '_transform.<locals>.<lambda>'

--- clip/clip.py
+++ clip/clip.py
@@ -68,11 +68,15 @@ def _download(url: str, root: str):
     return download_target
 
 
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")
+
+
 def _transform(n_px):
     return Compose([
         Resize(n_px, interpolation=BICUBIC),
         CenterCrop(n_px),
-        lambda image: image.convert("RGB"),
+        _convert_image_to_rgb,
         ToTensor(),
         Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
     ])
