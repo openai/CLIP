f9bc83733b5a3490b0be5e731f75bdfe5f276be4
Alireza Davoudi <davoudialireza@gmail.com>
2022-09-05 21:00:09 +0430

    Update model.py

--- clip/model.py
+++ clip/model.py
@@ -159,7 +159,7 @@ class LayerNorm(nn.LayerNorm):
 
     def forward(self, x: torch.Tensor):
         orig_type = x.dtype
-        ret = super().forward(x.type(torch.float32))
+        ret = super(LayerNorm).forward(x.type(torch.float32))
         return ret.type(orig_type)
 
 
