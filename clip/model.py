08fd72537481191ab0992d4976b47ff2e50c2252
ofernandesumojo <86738562+ofernandesumojo@users.noreply.github.com>
2023-08-28 15:18:20 -0500

    remove function def for torchscript conversion

--- clip/model.py
+++ clip/model.py
@@ -136,15 +136,11 @@ class ModifiedResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        def stem(x):
-            x = self.relu1(self.bn1(self.conv1(x)))
-            x = self.relu2(self.bn2(self.conv2(x)))
-            x = self.relu3(self.bn3(self.conv3(x)))
-            x = self.avgpool(x)
-            return x
-
         x = x.type(self.conv1.weight.dtype)
-        x = stem(x)
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.relu3(self.bn3(self.conv3(x)))
+        x = self.avgpool(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
