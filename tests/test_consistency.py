deb2964c2639c02644e724781ef231e4541780ec
PiotrBLL <piotr.sobieszczyk@bluelabellabs.com>
2024-12-19 02:05:11 +0100

    Add test - `test_hpu_support`

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -6,9 +6,28 @@ from PIL import Image
 import clip
 
 
-@pytest.mark.parametrize('model_name', clip.available_models())
+@pytest.mark.parametrize("model_name", clip.available_models())
 def test_consistency(model_name):
     device = "cpu"
+    jit_model, transform = clip.load(model_name, device=device, jit=True)
+    py_model, _ = clip.load(model_name, device=device, jit=False)
+
+    image = transform(Image.open("CLIP.png")).unsqueeze(0).to(device)
+    text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+
+    with torch.no_grad():
+        logits_per_image, _ = jit_model(image, text)
+        jit_probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+        logits_per_image, _ = py_model(image, text)
+        py_probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+    assert np.allclose(jit_probs, py_probs, atol=0.01, rtol=0.1)
+
+
+@pytest.mark.parametrize("model_name", clip.available_models())
+def test_hpu_support(model_name):
+    device = "hpu"
     jit_model, transform = clip.load(model_name, device=device, jit=True)
     py_model, _ = clip.load(model_name, device=device, jit=False)
 
