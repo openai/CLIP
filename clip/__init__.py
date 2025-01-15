3277251590d1584b37e8959300051a5b82352db7
Paula Derrenger <107626595+pderrenger@users.noreply.github.com>
2025-01-15 14:16:52 +0100

    Standardize license headers in Python files (#14)
    Automated merge of Ultralytics Code Refactor

--- clip/__init__.py
+++ clip/__init__.py
@@ -1,1 +1,3 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 from .clip import *
--- clip/clip.py
+++ clip/clip.py
@@ -1,3 +1,5 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 import hashlib
 import os
 import urllib
